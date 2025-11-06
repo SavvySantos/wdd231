5da60ced172bb08ee92e940dac626a7a255af270
SavvySantos <ssantos@byupathway.edu>
2025-11-06 15:16:41 -0700

    added more content to my page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,7 +1,37 @@
-const hamButton = document.querySelector('#ham-button');
-const navBar = document.querySelector('#nav-bar');
+async function getMembers() {
+    const response = await fetch('./data/members.json');
+    const data = await response.json();
+    displayMembers(data.members);
+}
 
-hamButton.addEventListener('click', () => {
-    navBar.classList.toggle('open');
-    hamButton.classList.toggle('open');
+function displayMembers(members) {
+    const memberDirectory = document.querySelector('main');
+    const membersContainer = document.createElement('div');
+    membersContainer.classList.add('members', 'grid');
+
+members.forEach(member => {
+    const card = document.createElement('section');
+
+    const image = document.createElement('img');
+    image.src = member.image;
+    image.alt = member.name;
+    image.loading = 'lazy';
+
+    const name = document.createElement('h2');
+    name.textContent = member.name;
+
+    const address = document.createElement('p');
+    address.textContent = member.address;
+
+    const website = document.createElement('a');
+    website.href = member.website;
+    website.textContent = 'Visit Website';
+
+    card.append(image, name, address, website);
+    membersContainer.appendChild(card);
+
 });
+    memberDirectory.appendChild(membersContainer);
+}
+
+    getMembers();
