cb3281fc8ce2d6b4b1638ec09d8531b41157fb97
SavvySantos <ssantos@byupathway.edu>
2025-11-26 19:44:00 -0700

    added image hover effect, and adjusted layout for large screen cards

--- chamber/scripts/visit-message.js
+++ chamber/scripts/visit-message.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const now = Date.now();
 
     if (!lastVisit) {
-        message.textContent = "Wecome! Let us know how we can help you!";
+        message.textContent = "Welcome! Let us know how we can help you!";
     } else {
         const lastTime = Number(lastVisit);
         const difference = now - lastTime;
@@ -15,7 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (daysBetween <1) {
             message.textContent = "Back so soon! Awesome!";
-        } else {
+        } else if (daysBetween === 1) {
+            message.textContent = "You last visited 1 day ago."
+        } 
+        
+        else {
             message.textContent = `You last visited ${daysBetween} days ago.`
         }
     }
