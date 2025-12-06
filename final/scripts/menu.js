c429e68dd3347797a9dacb5b7372d657e8f24ea4
SavvySantos <ssantos@byupathway.edu>
2025-12-06 15:58:32 -0700

    menu page

---
+++ final/scripts/menu.js
@@ -0,0 +1,54 @@
+async function loadMenu() {
+    const response = await fetch("data/menu.json");
+    const data = await response.json();
+
+    displayCategory("tacos", data.menu);
+    displayCategory("drinks", data.drinks);
+}
+
+function displayCategory(categoryId, items) {
+    const container = document.getElementById(categoryId);
+
+    items.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("menu-card");
+
+        let priceText = `$${item.price.toFixed(2)}`;
+        if(item.loadedPrice) {
+            priceText += ` / Loaded $${item.loadedPrice.toFixed(2)}`;
+        }
+
+        if (item.extraMeatPrice) {
+            priceText += ` (+$${item.extraMeatPrice.toFixed(2)} extra meat)`;
+        }
+
+        div.innerHTML = `
+            <span class="menu-name">${item.name}</span>
+            <span class="menu-price">${priceText}</span>
+            <span class="menu-desc">${item.description}</span>
+        `;
+        
+        
+        
+        
+        /*`
+        <h3>${item.name} - ${priceText}</h3>
+        <p>${item.description}</p>
+        `;*/
+
+        container.appendChild(div);
+
+
+
+
+
+       /* div.innerHTML = `
+        <h3>${item.name} - $${item.price.toFixed(2)}</h3>
+        <p>${item.description}</p>
+        `;
+
+        container.appendChild(div);*/
+    });
+}
+
+loadMenu();
